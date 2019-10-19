eb8d7eac07047e4ff500e4f25c0fddc0851c0da3
atemmel <temmeladam@gmail.com>
2019-10-19 20:23:45 +0000

    Fixade stdev & mean

--- main.cpp
+++ main.cpp
@@ -3,33 +3,63 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <cmath>
+
+template<typename T>
+T square(T value) {
+	return value * value;
+}
+
+using Burst = int;
+using Chrono = size_t;
 
 struct Process {
 	operator bool() {
 		return remainingBurst <= 0;
 	}
 
-	int totalBurst;
-	int remainingBurst;
+	Burst totalBurst;
+	Burst remainingBurst;
 };
 
 using Processes = std::vector<Process>;
-using Chrono = std::vector<size_t>;
+using Chronos = std::vector<Chrono>;
 
 struct Schedule {
+	double mean() const {
+		return std::accumulate(waitingTime.begin(), waitingTime.end(), 0.) 
+			/ static_cast<double>(waitingTime.size() );
+	}
+
+	double stdev() const {
+		const double my = mean();
+		double sigma = 0.;
+
+		for(double d : waitingTime) {
+			sigma += square(d - my);
+		}
+
+		sigma = std::sqrt(sigma / static_cast<double>(waitingTime.size() ) );
+
+
+		return sigma;
+	}
+
 	Processes processes;
-	Chrono waitingTime;
+	Chronos waitingTime;
 	size_t totalTime = 0;
 };
 
 struct Fifo {
 	void operator()(Schedule &schedule) {
-		
+		schedule.totalTime += schedule.processes.front().totalBurst;
+		schedule.waitingTime.push_back(schedule.totalTime - schedule.processes.front().totalBurst);
+		schedule.processes.erase(schedule.processes.begin() );
 	}
 };
 
 struct RoundRobin {
-	constexpr static int quantum = 4;
+	constexpr static Burst quantum = 4;
 
 	void operator()(Schedule &schedule) {
 		auto &processes = schedule.processes;
@@ -37,7 +67,7 @@ struct RoundRobin {
 		processes.front().remainingBurst -= quantum; //Applicera tidskvantum
 		schedule.totalTime += quantum;
 
-		if(int rem = processes.front().remainingBurst; rem < 0) {
+		if(Burst rem = processes.front().remainingBurst; rem < 0) {
 			schedule.totalTime += rem;
 		}
 
@@ -61,16 +91,16 @@ void runSchedule(Schedule &schedule, T &strategy) {
 	}
 }
 
-Schedule genSchedule(size_t n) {
+Schedule generateSchedule(size_t n) {
 	std::random_device rd;
 	std::mt19937 mt(rd() );
-	std::uniform_int_distribution<int> dist(1, 20);
+	std::uniform_int_distribution<Burst> dist(1, 20);
 
 	Schedule schedule;
 	schedule.processes.resize(n);
 
 	std::generate(schedule.processes.begin(), schedule.processes.end(), [&]() {
-		int x = dist(mt);
+		Burst x = dist(mt);
 		return Process{x, x};
 	});
 
@@ -78,10 +108,10 @@ Schedule genSchedule(size_t n) {
 }
 
 int main() {
-	size_t n = 3;
+	constexpr size_t n = 10e3;
 
 	Schedule scheduleA, scheduleB;
-	scheduleA = scheduleB = genSchedule(n);
+	scheduleA = scheduleB = generateSchedule(n);
 
 	auto printp = [](const Process pr) {
 		std::cout <<  pr.totalBurst << ' ' << pr.remainingBurst << '\n';
@@ -91,11 +121,23 @@ int main() {
 		std::cout <<  wt << '\n';
 	};
 
+	/*
 	std::for_each(scheduleA.processes.begin(), scheduleA.processes.end(), printp);
-	std::for_each(scheduleA.waitingTime.begin(), scheduleA.waitingTime.end(), printst);
 	puts("");
+	std::for_each(scheduleA.waitingTime.begin(), scheduleA.waitingTime.end(), printst);
+	std::for_each(scheduleB.processes.begin(), scheduleB.processes.end(), printp);
+	puts("");
+	std::for_each(scheduleB.waitingTime.begin(), scheduleB.waitingTime.end(), printst);
+	*/
+
 	RoundRobin rr;
 	runSchedule(scheduleA, rr);
-	std::for_each(scheduleA.waitingTime.begin(), scheduleA.waitingTime.end(), printst);
-	//std::for_each(scheduleB.processes.begin(), scheduleB.processes.end(), print);
+	Fifo fifo;
+	runSchedule(scheduleB, fifo);
+
+	std::cout << "N is " << n << '\n';
+	std::cout << "Round Robin mean: " << scheduleA.mean() << '\n';
+	std::cout << "Round Robin stdev: " << scheduleA.stdev() << '\n';
+	std::cout << "First-in First-out mean: " << scheduleB.mean() << '\n';
+	std::cout << "First-in First-out stdev: " << scheduleB.stdev() << '\n';
 }
