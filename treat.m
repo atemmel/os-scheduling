496aff503977159c98b3719f6223c04ce0cc2287
atemmel <temmeladam@gmail.com>
2019-10-21 15:49:00 +0200

    Mangefix

--- treat.m
+++ treat.m
@@ -22,16 +22,36 @@ y_fifo = pdf(pd_fifo, x_fifo);
 x_rr = 6000:7600;
 y_rr = pdf(pd_rr, x_rr);
 
-figure
+figure(1)
 hold on
-histogram(fifo, 'Normalization', 'pdf');
-line(x_fifo, y_fifo);
+p_fifo = histogram(fifo, 100);
+line(x_fifo, y_fifo * 8000);
 
-histogram(rr, 'Normalization', 'pdf');
-line(x_rr, y_rr);
+p_rr = histogram(rr, 100);
+line(x_rr, y_rr * 14000);
 
+xline(ci_fifo(1, 1), '-m');
+xline(ci_fifo(2, 1), '-m');
+
+xline(ci_rr(1, 1), '-m');
+xline(ci_rr(2, 1), '-m')
+
+%{
 xline(prctile(fifo, 5), '-m', '5%');
 xline(prctile(fifo, 95), '-m', '95%');
 
 xline(prctile(rr, 5), '-m', '5%');
 xline(prctile(rr, 95), '-m', '95%');
+%}
+hold off
+
+legend([p_fifo, p_rr], {'Fifo', 'Round Robin'})
+
+figure(2)
+tiledlayout(1,2)
+nexttile
+bar(diag([mu_fifo, mu_rr]), 'stacked')
+legend({'µ Fifo', 'µ Round Robin'})
+nexttile
+bar(diag([sigma_fifo ; sigma_rr]), 'stacked')
+legend({'std Fifo', 'std Round Robin'})
